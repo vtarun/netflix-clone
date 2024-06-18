ed11b8dc6a23b009c82592deec654e583bd4b2d8
vtarun <vivektarun89@gmail.com>
2024-06-19 01:12:51 +0530

    updated App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { Routes, Route, useNavigate} from 'react-router-dom';
+
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Player from './pages/Player/Player';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+
 
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(()=>{
+    onAuthStateChanged(auth, async (user)=>{
+      if(user){
+        navigate("/");
+      }else{
+        navigate("login");
+      }
+    })
+  },[]);
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/player/:id' element={<Player/>} />
+      </Routes>
     </div>
   )
 }
